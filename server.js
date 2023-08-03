12de36691b38268e25a89f20c29e348e81c857f9
Sean Fowers <sean.fowers@griffithuni.edu.au>
2023-08-03 22:19:03 +1000

    Workshop complete

--- server.js
+++ server.js
@@ -2,7 +2,10 @@ var express = require('express');
 var formidable = require('formidable');
 var app = express();
 var http = require('http').Server(app);
+var bodyparser = require('body-parser');
+const bodyParser = require('body-parser');
 
+app.use(bodyParser.json());
 app.use(express.static(__dirname + '/www'));
 
 let server = http.listen(3000, function () {
@@ -13,8 +16,42 @@ let server = http.listen(3000, function () {
 });
 
 app.get('/', function(req, res) {
-    res.sendFile(__dirname + '/www/index.html')
+    res.sendFile(__dirname + '/www/loginform.html')
 });
+
+app.get('/account', function(req, res) {
+    res.sendFile(__dirname + '/www/account.html')
+});
+
+app.post('/api/login', function(req, res) {
+    let users = [{'email':'abc@gmail.com', 'password':'123'}, 
+                {'email':'def@gmail.com', 'password':'123'}, 
+                {'email':'ghi@gmail.com', 'password':'123'}];
+
+    if(!req.body) {
+        return res.sendStatus(400);
+    }
+
+    var customer = {};
+    customer.email = req.body.email;
+    customer.upwd = req.body.upwd;
+    customer.valid = false;
+    
+    for (let i=0; i<users.length;i++) {
+        if(req.body.email == users[i].email && req.body.upwd == users[i].upwd) {
+            customer.valid = true;
+        }
+    }
+
+    res.send(customer);
+    //res.sendFile(__dirname + '/www/account.html')
+});
+
+app.get('/test', function(req, res) {
+    res.sendFile(__dirname + '/www/test.html')
+});
+
+/*
 app.post('/', function(req, res) {
     var form = new formidable.IncomingForm();
     form.parse(req);
@@ -27,11 +64,4 @@ app.post('/', function(req, res) {
 
     res.sendFile(__dirname + '/www/index.html')
 });
-
-app.get('/test', function(req, res) {
-    res.sendFile(__dirname + '/www/test.html')
-});
-
-app.get('/account', function(req, res) {
-    res.sendFile(__dirname + '/www/account.html')
-});
+*/
--- www/script.js
+++ www/script.js
@@ -1,22 +1,22 @@
 $( document ).ready(function() {
     console.log("ready!");
     $("#loginform").submit(function(event){
-        event.presentDefault();
+        event.preventDefault();
         ajaxPost();
     });
 
     function ajaxPost(){
         var formData = {
-            email: $("email").val(),
-            password: $("password").val()
+            email: $("#email").val(),
+            password: $("#upwd").val()
         }
         $.ajax({
-            type: "POST",
-            contentType: "application/json",
-            url: window.location + "api/login",
-            data: JSON.stringify(formData),
-            dataType: 'json',
-            success: function(customer) {
+            type : "POST",
+            contentType : "application/json",
+            url : window.location + "api/login",
+            data : JSON.stringify(formData),
+            dataType : 'json',
+            success : function(customer) {
                 if(customer.valid == true) {
                     $('#loginform').addClass("success");
                     $('#loginform').removeClass("failure");
@@ -25,10 +25,10 @@ $( document ).ready(function() {
                     $('#loginform').addClass("failure");
                 }
                 $('#postResultDiv').html("<p>" + "Post Successfull! <br>" + "Email Address: " + 
-                                    customer.email + "<br>" + "Password: " + customer.password +
-                                    "<br>" + "Valid User: " + customer.valid + "</p>")
+                                    customer.email + "<br>" + "Password: " + customer.upwd +
+                                    "<br>" + "Valid User: " + customer.valid + "</p>");
             },
-            error: function(e) {
+            error : function(e) {
                 alert("Error!");
                 console.log("ERROR: ", e);
             }
